014710545e289c08cacf8282830e0987d32e3a12
sjlee <thetomatoaddict@gmail.com>
2022-11-11 21:17:22 +0900

    created table

--- myTable/ViewController.swift
+++ myTable/ViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+
+    @IBOutlet weak var mytable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
 
 }
